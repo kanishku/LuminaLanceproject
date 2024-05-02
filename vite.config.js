2cf2a1ad4d349161b07a8607809c94c203c37c7f
kanishku <k6473870@gmail.com>
2024-05-02 21:55:25 +0530

    LuminaLanceproject

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// vite.config.js
+
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/https://kanishku.github.io/LuminaLanceproject/',
 })
